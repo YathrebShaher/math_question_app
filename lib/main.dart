21e919f87099d78d841c13b712f6dda228925d0e
Yathreb Bashandy <yathrebshahaer@gmail.com>
2021-08-13 01:12:36 +0200

    init state

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:math_question_app/app/pages/add_question.page.dart';
+import 'package:workmanager/workmanager.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Question App',
+      theme: ThemeData(
+        primaryColor: Colors.pink,
+        accentColor: Colors.white30,
+        primarySwatch: Colors.pink,
+      ),
+      routes: {
+        AddQuestionPage.ROUTE_NAME: (context) => AddQuestionPage(),
+      },
+      initialRoute: AddQuestionPage.ROUTE_NAME,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
